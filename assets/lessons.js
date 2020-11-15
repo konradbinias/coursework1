fe432d56358524529425e546f137fda92ae44d7e
konradbinias <konrad.binias@gmail.com>
2020-11-15 23:11:11 +0100

    Read data from external JSON file

---
+++ assets/lessons.js
@@ -0,0 +1,62 @@
+export default [
+    {
+    subject: 'Math',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'Biology',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'French',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'Football',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'Guitar',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'History',
+    location: 'London',
+    price: 200,
+    spaces: 5
+    },
+    {
+    subject: 'Chess',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'Basketball',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'German',
+    location: 'London',
+    price: 100,
+    spaces: 5
+    },
+    {
+    subject: 'English',
+    location: 'London',
+    price: 150,
+    spaces: 5
+    }
+]
