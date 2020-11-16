4cdfbf6362b4b358e9c53439b9e37f0698b55212
konradbinias <konrad.binias@gmail.com>
2020-11-16 15:57:13 +0100

    Show the lesson icon

--- assets/lessons.js
+++ assets/lessons.js
@@ -2,61 +2,71 @@ export default [
     {
     subject: 'Math',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 90,
+    spaces: 5,
+    image: 'math.png'
     },
     {
     subject: 'Biology',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 105,
+    spaces: 5,
+    image: 'biology.png'
     },
     {
     subject: 'French',
     location: 'London',
     price: 100,
-    spaces: 5
+    spaces: 5,
+    image: 'french.png'
     },
     {
     subject: 'Football',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 110,
+    spaces: 5,
+    image: 'football.png'
     },
     {
     subject: 'Guitar',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 70,
+    spaces: 5,
+    image: 'guitar.png'
     },
     {
     subject: 'History',
     location: 'London',
-    price: 200,
-    spaces: 5
+    price: 80,
+    spaces: 5,
+    image: 'hist.png'
     },
     {
     subject: 'Chess',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 35,
+    spaces: 5,
+    image: 'chess.png'
     },
     {
     subject: 'Basketball',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 95,
+    spaces: 5,
+    image: 'basketball.png'
     },
     {
     subject: 'German',
     location: 'London',
-    price: 100,
-    spaces: 5
+    price: 135,
+    spaces: 5,
+    image: 'germany.png'
     },
     {
     subject: 'English',
     location: 'London',
     price: 150,
-    spaces: 5
+    spaces: 5,
+    image: 'english.png'
     }
 ]
