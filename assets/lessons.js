1dae83a669f0bd3f40409d95d70389cea77bb5bf
konradbinias <konrad.binias@gmail.com>
2020-11-17 22:35:38 +0100

    Add basket button

--- assets/lessons.js
+++ assets/lessons.js
@@ -1,70 +1,70 @@
 export default [
     {
     subject: 'Math',
-    location: 'London',
+    location: 'Greenwich',
     price: 90,
     spaces: 5,
     image: 'math.png'
     },
     {
     subject: 'Biology',
-    location: 'London',
+    location: 'Lewisham',
     price: 105,
     spaces: 5,
     image: 'biology.png'
     },
     {
     subject: 'French',
-    location: 'London',
+    location: 'Hendon',
     price: 100,
     spaces: 5,
     image: 'french.png'
     },
     {
     subject: 'Football',
-    location: 'London',
+    location: 'Chelsea',
     price: 110,
     spaces: 5,
     image: 'football.png'
     },
     {
     subject: 'Guitar',
-    location: 'London',
+    location: 'Camden',
     price: 70,
     spaces: 5,
     image: 'guitar.png'
     },
     {
     subject: 'History',
-    location: 'London',
+    location: 'Croydon',
     price: 80,
     spaces: 5,
     image: 'hist.png'
     },
     {
     subject: 'Chess',
-    location: 'London',
+    location: 'Brent',
     price: 35,
     spaces: 5,
     image: 'chess.png'
     },
     {
     subject: 'Basketball',
-    location: 'London',
+    location: 'Lambeth',
     price: 95,
     spaces: 5,
     image: 'basketball.png'
     },
     {
     subject: 'German',
-    location: 'London',
+    location: 'Newham',
     price: 135,
     spaces: 5,
     image: 'germany.png'
     },
     {
     subject: 'English',
-    location: 'London',
+    location: 'Hackney',
     price: 150,
     spaces: 5,
     image: 'english.png'
