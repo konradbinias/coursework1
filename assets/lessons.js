7717fca20a08c389078f453328857d90c06c0460
konradbinias <konrad.binias@gmail.com>
2020-11-18 22:16:52 +0100

    Passing chosen lessons between pages

--- assets/lessons.js
+++ assets/lessons.js
@@ -1,72 +1,72 @@
 export default [
     {
-    subject: 'Math',
-    location: 'Greenwich',
-    price: 90,
-    spaces: 5,
-    image: 'math.png'
+        subject: 'Math',
+        location: 'Greenwich',
+        price: 90,
+        spaces: 5,
+        image: 'math.png'
     },
     {
-    subject: 'Biology',
-    location: 'Lewisham',
-    price: 105,
-    spaces: 5,
-    image: 'biology.png'
+        subject: 'Biology',
+        location: 'Lewisham',
+        price: 105,
+        spaces: 5,
+        image: 'biology.png'
     },
     {
-    subject: 'French',
-    location: 'Hendon',
-    price: 100,
-    spaces: 5,
-    image: 'french.png'
+        subject: 'French',
+        location: 'Hendon',
+        price: 100,
+        spaces: 5,
+        image: 'french.png'
     },
     {
-    subject: 'Football',
-    location: 'Chelsea',
-    price: 110,
-    spaces: 5,
-    image: 'football.png'
+        subject: 'Football',
+        location: 'Chelsea',
+        price: 110,
+        spaces: 5,
+        image: 'football.png'
     },
     {
-    subject: 'Guitar',
-    location: 'Camden',
-    price: 70,
-    spaces: 5,
-    image: 'guitar.png'
+        subject: 'Guitar',
+        location: 'Camden',
+        price: 70,
+        spaces: 5,
+        image: 'guitar.png'
     },
     {
-    subject: 'History',
-    location: 'Croydon',
-    price: 80,
-    spaces: 5,
-    image: 'hist.png'
+        subject: 'History',
+        location: 'Croydon',
+        price: 80,
+        spaces: 5,
+        image: 'hist.png'
     },
     {
-    subject: 'Chess',
-    location: 'Brent',
-    price: 35,
-    spaces: 5,
-    image: 'chess.png'
+        subject: 'Chess',
+        location: 'Brent',
+        price: 35,
+        spaces: 5,
+        image: 'chess.png'
     },
     {
-    subject: 'Basketball',
-    location: 'Lambeth',
-    price: 95,
-    spaces: 5,
-    image: 'basketball.png'
+        subject: 'Basketball',
+        location: 'Lambeth',
+        price: 95,
+        spaces: 5,
+        image: 'basketball.png'
     },
     {
-    subject: 'German',
-    location: 'Newham',
-    price: 135,
-    spaces: 5,
-    image: 'germany.png'
+        subject: 'German',
+        location: 'Newham',
+        price: 135,
+        spaces: 5,
+        image: 'germany.png'
     },
     {
-    subject: 'English',
-    location: 'Hackney',
-    price: 150,
-    spaces: 5,
-    image: 'english.png'
+        subject: 'English',
+        location: 'Hackney',
+        price: 150,
+        spaces: 5,
+        image: 'english.png'
     }
 ]
